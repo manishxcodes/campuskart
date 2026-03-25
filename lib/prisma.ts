a8993741f016d81f8688346eac47157ebf2fc972
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-03-26 00:10:53 +0530

    centralize Prisma client initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,15 @@
-import  { PrismaClient }  from "../generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalPrisma = global as unknown as {
-    prisma: PrismaClient
-};
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalPrisma.prisma || new PrismaClient();
+function createPrismaClient() {
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+  });
+  return new PrismaClient({ adapter });
+}
 
-if(process.env.NODE_ENV !== "production") 
-    globalPrisma.prisma = prisma
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
