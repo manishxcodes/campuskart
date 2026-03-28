2ef8c2a64170fcc94d7a50c9aff4876ceb1d601a
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-03-28 22:20:59 +0530

    add tooltip and toaster in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "@/components/providers/sesssion-provider";
+import { Toaster } from "sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider> {children}</SessionProvider>
+        <SessionProvider>
+          {" "}
+          <TooltipProvider>{children}</TooltipProvider>
+          <Toaster position="top-center" richColors />
+        </SessionProvider>
       </body>
     </html>
   );
