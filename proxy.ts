e35875d00414a8b4b54af61cb02e62ee56242cbb
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-03-28 22:22:29 +0530

    add middleware for auth

---
+++ proxy.ts
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./lib/auth.config";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth ((req) => {
+    const { nextUrl, auth: session } = req;
+    const isLoggedIn = !!session;
+
+    const isAuthPage = nextUrl.pathname.startsWith("/signin") || nextUrl.pathname.startsWith("/signup") ;
+    const isPublicRoute = nextUrl.pathname === "/";
+    const isApiAuthRoute = nextUrl.pathname.startsWith("/api/auth");
+
+    if (isApiAuthRoute) return;
+
+    if (isLoggedIn && isAuthPage) {
+        return Response.redirect(new URL("/", nextUrl));
+    }
+
+    if(!isLoggedIn && !isAuthPage && !isPublicRoute) {
+        const callbackUrl = encodeURIComponent(nextUrl.pathname);
+        return Response.redirect(
+            new URL(`/signin?callbackUrl=${callbackUrl}`, nextUrl)
+        );
+    }
+
+    return ;
+});
+
+export const config = {
+  matcher: [
+    "/((?!api/auth|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
