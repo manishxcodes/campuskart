c18ed740122a8ce8013a380110077a6a246cd651
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-04-04 23:18:23 +0530

    feat: add logic to handle page navigation of  previously logged in user

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,51 @@
+"use client";
+
+import { Navbar } from "@/components/navbar";
 import { Button } from "@/components/ui/button";
 import { Container } from "@/components/ui/container";
+import { Loader } from "lucide-react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default function LandingPage() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Loader className="animate-spin" />
+        <span>loading</span>
+      </div>
+    );
+  }
+
+  if (session) {
+    redirect("/home");
+  }
+
   return (
-    <div className="flex h-screen items-center justify-center font-sans">
-      <Container>
-        <div className="flex flex-col items-center text-center">
-          <h1 className="text-4xl md:text-6xl font-semibold tracking-tight">
-            Your Campus <br />
-            Your Marketplace
-          </h1>
+    <>
+      <Navbar />
+      <div className="flex h-screen items-center justify-center ">
+        <Container>
+          <div className="flex flex-col items-center text-center">
+            <h1 className="text-4xl md:text-6xl font-semibold tracking-tight">
+              Your Campus <br />
+              Your Marketplace
+            </h1>
 
-          <p className="mt-5 text-sm md:text-base text-zinc-500">
-            Connect with fellow students to buy, sell, <br />
-            and save on everyday essentials.
-          </p>
+            <p className="mt-5 text-sm md:text-base text-zinc-500">
+              Connect with fellow students to buy, sell, <br />
+              and save on everyday essentials.
+            </p>
 
-          <Button className="mt-8">Start Shopping</Button>
-        </div>
-      </Container>
-    </div>
+            <Link href={"/signup"}>
+              <Button className="mt-8">Start Shopping</Button>
+            </Link>
+          </div>
+        </Container>
+      </div>
+    </>
   );
 }
