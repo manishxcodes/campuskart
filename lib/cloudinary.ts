66f4e1f803e58eb5a57616cb071dc3e57aa18b0a
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-04-06 17:58:01 +0530

    feat: integrate cloudinary for image uplaod

---
+++ lib/cloudinary.ts
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
+
+export async function uploadImageToCloudinary (file: string, folder: string = "campuskart/avatars") {
+    const result = await cloudinary.uploader.upload(file, {
+        folder,
+        transformation: [
+            { width: 400, height: 400, crop: "fill", gravity: "face" },
+        ],
+    });
+
+    return {
+        url: result.secure_url,
+        publicId: result.public_id
+    };
+}
+
+export async function deleteImage(publicId: string) {
+    await cloudinary.uploader.destroy(publicId);
+}
