c6ae59505c5bab6c3e2d2556d73a402189bbb0ae
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-04-25 19:05:16 +0530

    feat: add home page

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -3,7 +3,11 @@
 import { useSession } from "next-auth/react";
 
 export default function Hello() {
-  const session = useSession();
-  console.log(session.data?.user.email);
-  return <div>Hello</div>;
+  const { data: session } = useSession();
+  console.log(session?.user.email);
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
 }
