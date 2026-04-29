b1fc6b71b44c5f2572e1f9eb7db06a248e93be6e
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-04-29 23:31:09 +0530

    feat: add isAdmin field in Session for type safety

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -5,6 +5,7 @@ declare module "next-auth" {
         user: {
             id: string;
             isProfileCompleted: boolean;
+            isAdmin: boolean;
         } & DefaultSession["user"];
     }
 }
